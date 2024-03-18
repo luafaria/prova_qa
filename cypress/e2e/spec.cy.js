1107637d8762ef0328c8f19b3611667af75532c6
luafaria <luciane.faria@hotmail.com>
2024-03-18 17:17:06 -0300

    projeto cypress trello vfinal

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -17,8 +17,6 @@ describe('Automação do Trello', () => {
     }).then((response) => {
       expect(response.status).to.eq(200)
       boardId = response.body.id
-      /*const responseString = JSON.stringify(response);
-      cy.log(responseString);*/
     })
   })
 
